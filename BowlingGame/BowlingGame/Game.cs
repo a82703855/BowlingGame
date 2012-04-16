49d8fcc9d36a8f491ec4ac81851a561beee55dbb
chenshuhui <a82703855@126.com>
2012-04-16 22:56:18 +0800

    Signed-off-by: chenshuhui <a82703855@126.com>

---
+++ BowlingGame/BowlingGame/Game.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BowlingGame
+{
+    public class Game
+    {
+        private int score = 0;
+        private int[] rolls = new int[21];
+        private int currentRoll = 0;
+        private int currentFrame = 1;
+        private bool isFirstRoll = true;
+
+        public void roll(int pins)
+        {
+
+           // score += pins;
+            rolls[currentRoll++] = pins;
+            AdjustCurrentFrame(pins);
+
+        }
+
+        private void AdjustCurrentFrame(int pins)
+        {
+            if (isFirstRoll)
+            {
+                if (pins == 10)
+                {
+                    currentFrame++;
+                }
+                else
+                {
+                    isFirstRoll = false;
+                }
+            }
+            else
+            {
+                isFirstRoll = true;
+                currentFrame++;
+            }
+            if (currentFrame > 11)
+            {
+                currentFrame = 11;
+            }
+        }
+
+        public int Score()
+        {
+            //int score = 0;
+            //int frameIndex = 0;
+            //for (int frame = 0; frame < 10; frame++)
+            //{
+            //    if (isStrike(frameIndex))//strike
+            //    {
+            //        score = 10 + strikeBonus(frameIndex);
+            //        frameIndex++;
+            //    }
+            //    else if (isSpare(frameIndex))//补中spare
+            //    {
+            //        score += 10 + spareBonus(frameIndex);
+            //        frameIndex += 2;
+            //    }
+            //    else
+            //    {
+            //        score += sumOfBallsInFrame(frameIndex);
+            //        frameIndex += 2;
+            //    }
+                
+                
+            //}
+            //return score;
+
+            return ScoreForFrame(CurrentFrame - 1);
+        }
+
+        private int sumOfBallsInFrame(int frameIndex)
+        {
+            return rolls[frameIndex] + rolls[frameIndex + 1];
+        }
+
+        private int spareBonus(int frameIndex)
+        {
+            return rolls[frameIndex + 2];
+        }
+
+        private int strikeBonus(int frameIndex)
+        {
+            return rolls[frameIndex + 1] + rolls[frameIndex + 2];
+
+        }
+
+        private bool isStrike(int frameIndex)
+        {
+            return rolls[frameIndex] == 10;
+        }
+
+        private bool isSpare(int frameIndex)
+        {
+            return rolls[frameIndex] + rolls[frameIndex + 1] == 10;
+        }
+
+        public int ScoreForFrame(int theFrame)
+        {
+            int score1 = 0;
+            int ball = 0;
+            for (int currentFrame = 0; currentFrame < theFrame; currentFrame++)
+            {
+                int firstRoll = rolls[ball++];
+                if (firstRoll == 10)
+                {
+                    score1 += 10 + rolls[ball] + rolls[ball + 1];
+                }
+                else
+                {
+                    int secondRoll = rolls[ball++];
+                    int frameScore = firstRoll + secondRoll;
+                    if (frameScore == 10)
+                    {
+                        score1 += frameScore + rolls[ball];
+                    }
+                    else
+                    {
+                        score1 += frameScore;
+                    }
+                }
+            }
+            return score1;
+        }
+
+        public int CurrentFrame 
+        { 
+            get { return currentFrame; } 
+        }
+    }
+}
---
+++ BowlingGame/BowlingGameTest/GameTest.cs
@@ -0,0 +1,139 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BowlingGame;
+
+
+
+[TestClass]
+public class BowlingGameTest
+{
+    private Game g;
+
+    public BowlingGameTest()
+    {
+        g = new Game();
+    }
+
+
+
+
+    [TestMethod]
+    public void testGutterGame()
+    {
+        // Game g = new Game();
+        //int n = 20;
+        //int pins = 0;
+        rollMany(20, 0);
+        Assert.AreEqual(0, g.Score());
+    }
+
+    private void rollMany(int n, int pins)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            g.roll(pins);
+        }
+    }
+
+    [TestMethod]
+    public void testAllOnes()
+    {
+        rollMany(20, 1);
+        Assert.AreEqual(20, g.Score());
+    }
+
+    [TestMethod]
+    public void testRolls()
+    {
+
+        g.roll(5);
+        g.roll(4);
+        g.roll(7);
+        g.roll(2);
+        Assert.AreEqual(18, g.Score());
+        Assert.AreEqual(9, g.ScoreForFrame(1));
+        Assert.AreEqual(18, g.ScoreForFrame(2));
+        Assert.AreEqual(3, g.CurrentFrame);
+    }
+
+    [TestMethod]
+    public void testOneSpare()
+    {
+        roolSpare();
+        g.roll(3);
+
+        Assert.AreEqual(13, g.Score());
+        Assert.AreEqual(13, g.ScoreForFrame(1));
+        Assert.AreEqual(2, g.CurrentFrame);
+    }
+    [TestMethod]
+    public void testFrameAfterSpare()
+    {
+        roolSpare();
+        g.roll(3);
+        g.roll(2);
+
+        Assert.AreEqual(18, g.Score());
+        Assert.AreEqual(13, g.ScoreForFrame(1));
+        Assert.AreEqual(18, g.ScoreForFrame(2));
+
+        Assert.AreEqual(3, g.CurrentFrame);
+    }
+
+    [TestMethod]
+    public void testOneStrike()
+    {
+        rollStrike(); //全中strike
+        g.roll(3);
+        g.roll(4);
+        rollMany(16, 0);
+        Assert.AreEqual(24, g.Score());
+    }
+
+    [TestMethod]
+    public void testHeartBreak()
+    {
+        for (int i = 0; i < 11; i++)
+        {
+            g.roll(10);
+        }
+        g.roll(9);
+        Assert.AreEqual(299, g.Score());
+    }
+
+    private void rollStrike()
+    {
+        g.roll(10);
+    }
+
+    private void roolSpare()
+    {
+        g.roll(5);
+        g.roll(5);
+    }
+    [TestMethod]
+    public void TestPerfectGame()
+    {
+        for (int i = 0; i < 12; i++)
+        {
+            g.roll(10);
+        }
+        Assert.AreEqual(300, g.Score());
+        Assert.AreEqual(11, g.CurrentFrame);
+
+
+
+    }
+    [TestMethod]
+    public void TestTenthTrameSpare()
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            g.roll(10);
+        }
+        g.roll(9);
+        g.roll(1);
+        g.roll(1);
+        Assert.AreEqual(270, g.Score());
+    }
+
+}
